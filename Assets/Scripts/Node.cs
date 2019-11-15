0bbf30d0ddfdf224d7f4610aae4cd54d152dd9bc
Jamie Covell <jc217189@falmouth.ac.uk>
2019-11-15 10:21:10 +0000

    wehh

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Node
+{
+
+    [SerializeField] Node previousNode;
+    [SerializeField] Node[] childNodes;
+    [SerializeField] NodeType type;
+
+}
+
+public enum NodeType
+{
+    leaf,
+    sequence,
+    selector
+}
